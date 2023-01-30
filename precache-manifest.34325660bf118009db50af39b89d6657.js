941dbd577302bfb26c9ae206fc09ab9dc8283e3d
Mrinmoy Goswami <you@example.com>
2023-01-30 23:41:16 +0530

    Updates

---
+++ precache-manifest.34325660bf118009db50af39b89d6657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4056a1249a3423706a93",
+    "url": "/Portfolio/static/css/main.b92496cd.chunk.css"
+  },
+  {
+    "revision": "4056a1249a3423706a93",
+    "url": "/Portfolio/static/js/main.4056a124.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/Portfolio/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "e61b3e8c48b6cea39087",
+    "url": "/Portfolio/static/js/2.e61b3e8c.chunk.js"
+  },
+  {
+    "revision": "9407d0ea190bd69006fe",
+    "url": "/Portfolio/static/js/runtime~main.9407d0ea.js"
+  },
+  {
+    "revision": "2a4053fecf28f03ec2001da4d6860e3e",
+    "url": "/Portfolio/index.html"
+  }
+];
